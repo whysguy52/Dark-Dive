fff7371724b2fb33a181554d89bd685616eb71ce
Ben Eng <whysguy52@gmail.com>
2021-04-26 20:01:11 -0500

    FINAL
    audio and everything is fixed

--- Models/Submarine/Propeller.gd
+++ Models/Submarine/Propeller.gd
@@ -2,11 +2,13 @@ extends MeshInstance
 
 
 var isMoving
+var PropellerAudio
+var audioIsPlaying = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	isMoving = false
+	PropellerAudio = get_node("PropellerAudio")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +18,12 @@ func _ready():
 
 func spin(movement: Vector3):
 	if movement.length() != 0 and movement.z <= 0:
+		
 		rotate(Vector3(0,0,1), -0.25)
 	elif movement.length() != 0 and movement.z > 0:
 		rotate(Vector3(0,0,1), 0.25)
 		
+	if movement.length() != 0 and !PropellerAudio.playing:
+		PropellerAudio.play()
+	elif movement.length() == 0 and PropellerAudio.playing:
+		PropellerAudio.stop()
